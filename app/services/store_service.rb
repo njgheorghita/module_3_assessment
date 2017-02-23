dfbd5272041aec0ca0cfa8f440aca5fa49a41dc6
njgheorghita <njgheorghita@email.wm.edu>
2017-02-23 11:26:38 -0700

    Refactor faraday call in store service

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,7 +1,17 @@
 class StoreService
   def self.search_by_zip
-    conn = Faraday.new(:url => 'https://api.bestbuy.com') 
-    response = conn.get("/v1/stores(area(80204,25))?format=json&apiKey=#{ENV['BEST_BUY_API_KEY']}")
-    JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(faraday_response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(:url => 'https://api.bestbuy.com')
+  end
+
+  def self.faraday_response
+    conn.get do |req|                    
+      req.url '/v1/stores(area(80204,25))'
+      req.params['format'] = 'json'
+      req.params['apiKey'] = ENV['BEST_BUY_API_KEY']
+    end
   end
 end
