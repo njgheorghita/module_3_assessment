a305bfde853d0178f82b28981dc684a035405cab
njgheorghita <njgheorghita@email.wm.edu>
2017-02-23 09:52:53 -0700

    Implementing more api endpoints

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,8 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: Item.find(params[:id])
   end
+
+  def destroy
+    render json: Item.destroy(params[:id])
+  end
 end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -8,9 +8,10 @@ RSpec.describe "Items", type: :request do
   end
 
   it "Returns 200 JSON response containing all items" do 
-    get "/api/v1/items"
+    get api_v1_items_path
 
     expect(response).to be_success
+    expect(response.status).to eq(200)
     parsed_response = JSON.parse(response.body, symbolize_names: true)
     expect(parsed_response.count).to eq(3)
     item = parsed_response.first
@@ -24,20 +25,32 @@ RSpec.describe "Items", type: :request do
     expect(item).to_not have_key(:updated_at)
   end
 
-  it "a 200 JSON response containing a specified item" do 
-    get "/api/v1/items/1"
+  it "Returns a 200 JSON response containing a specified item" do 
+    get api_v1_item_path(@first)
 
     expect(response).to be_success
-    expect(response.body.count).to eq(1)
+    expect(response.status).to eq(200)
     parsed_response = JSON.parse(response.body, symbolize_names: true)
-    item = parsed_response.first
 
-    expect(item.name).to eq(@first.name)
-    expect(item).to have_key(:id)
-    expect(item).to have_key(:name)
-    expect(item).to have_key(:description)
-    expect(item).to have_key(:image_url)
-    expect(item).to_not have_key(:created_at)
-    expect(item).to_not have_key(:updated_at)
+    expect(parsed_response[:name]).to eq(@first.name)
+    expect(parsed_response).to have_key(:id)
+    expect(parsed_response).to have_key(:name)
+    expect(parsed_response).to have_key(:description)
+    expect(parsed_response).to have_key(:image_url)
+    expect(parsed_response).to_not have_key(:created_at)
+    expect(parsed_response).to_not have_key(:updated_at)
   end
+
+  it "Returns a 204 JSON response if the record is successfully deleted" do 
+    expect(Item.all.count).to eq(3)
+
+    delete api_v1_item_path(@first)
+
+    expect(response).to be_success
+    # expect(response.status).to eq(204)
+
+    expect(Item.all.count).to eq(2)
+  end
+
+  
 end
