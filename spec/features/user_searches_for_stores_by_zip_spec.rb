0de42965949280cf41a60a3074a5f1c1972e9642
njgheorghita <njgheorghita@email.wm.edu>
2017-02-23 10:40:11 -0700

    Postman, view, search form
    used postman to find api path, implemented search field into home page

--- spec/features/user_searches_for_stores_by_zip_spec.rb
+++ spec/features/user_searches_for_stores_by_zip_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe do
   context "As a user" do 
-    scenario "I can search for stores by zip" do 
-      it "returns stores within 25 miles" do 
-        # When I visit "/"
-        visit root_path
-        # And I fill in a search box with "80202" and click "search"
-        fill_in "search", with: 80202
-        click_on "search"
-        # Then my current path should be "/search" (ignoring params)
-        expect(current_path).to eq("/search")
-        # And I should see stores within 25 miles of 80202
-        # And I should see a message that says "16 Total Stores"
-        expect(page).to have_content("16 Total Stores")
-        # And I should see exactly 10 results
-        expect(page).to have_selector(".store", count: 10)
-        # And I should see the long name, city, distance, phone number and store type for each of the 10 results
-        within first.("store") do
-          expect(page).to have_content("StoreName")
-        end
+    scenario "I can search for stores within 25 miles by zip" do 
+      # When I visit "/"
+      visit root_path
+      # And I fill in a search box with "80202" and click "search"
+      fill_in "search", with: 80202
+      click_on "search"
+      # Then my current path should be "/search" (ignoring params)
+      expect(current_path).to eq("/search")
+      # And I should see stores within 25 miles of 80202
+      # And I should see a message that says "16 Total Stores"
+      expect(page).to have_content("16 Total Stores")
+      # And I should see exactly 10 results
+      expect(page).to have_selector(".store", count: 10)
+      # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+      within first.("store") do
+        expect(page).to have_content("StoreName")
       end
     end
   end
