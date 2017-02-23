b3c8eafc54951c621a16b1b6d4e2e01ed1c302df
njgheorghita <njgheorghita@email.wm.edu>
2017-02-23 12:04:23 -0700

    Remove byebug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    byebug
     response = StoreService.search_by_zip
     @total_count = response[:total]
     @stores = response[:stores].map do |store|
